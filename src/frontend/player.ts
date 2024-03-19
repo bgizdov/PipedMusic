33cb935e31a762a278e070cce6d2ddad777d6e93
Filip Znachor <filip@znachor.cz>
2024-03-19 18:01:02 +0100

    Added mediaSession player support

--- src/frontend/player.ts
+++ src/frontend/player.ts
@@ -10,20 +10,28 @@ export class Player {
 
 	public constructor() {
 		this.el = document.createElement("audio");
+		this.hookMediaSession();
 	}
 
 	public setPlaying(video: RichVideo) {
 		this.retryCount = 0;
 		this.playing = video;
 		this.el.src = video.stream;
+		this.setMediaSession();
 	}
 
 	public play() {
 		this.el.play();
+		if (navigator.mediaSession) {
+			navigator.mediaSession.playbackState = "playing";
+		}
 	}
 
 	public pause() {
 		this.el.pause();
+		if (navigator.mediaSession) {
+			navigator.mediaSession.playbackState = "paused";
+		}
 	}
 
 	public playPause() {
@@ -31,7 +39,11 @@ export class Player {
 	}
 
 	public seek(time: number) {
-		this.el.currentTime = time;
+		this.el.currentTime = Math.max(Math.min(time, this.el.duration), 0);
+	}
+
+	public seekRel(relTime: number) {
+		this.seek(this.el.currentTime + relTime);
 	}
 
 	public setVolume(volume: number) {
@@ -46,6 +58,44 @@ export class Player {
 			}, 1000);
 		}
 		this.retryCount++;
+	}
+
+	public setMediaSession() {
+		if (!navigator.mediaSession || !this.playing) return;
+		navigator.mediaSession.metadata = new MediaMetadata({
+			title: this.playing.title,
+			artist: this.playing.author,
+			album: "",
+			artwork: [
+				{src: this.playing.thumbnail}
+			]
+		});
+	}
+
+	private hookMediaSession() {
+		if (navigator.mediaSession) {
+			this.el.addEventListener("durationchange", () => this.setPositionState());
+			this.el.addEventListener("timeupdate", () => this.setPositionState());
+			navigator.mediaSession.setActionHandler("play", () => this.play());
+			navigator.mediaSession.setActionHandler("pause", () => this.pause());
+			navigator.mediaSession.setActionHandler("seekto", (a) => {
+				if (a.seekTime) this.seek(a.seekTime);
+			});
+			navigator.mediaSession.setActionHandler("seekforward", (a) => {
+				this.seekRel(a.seekOffset ? +a.seekOffset : +10);
+			});
+			navigator.mediaSession.setActionHandler("seekbackward", (a) => {
+				this.seekRel(a.seekOffset ? -a.seekOffset : -10);
+			});
+		}
+	}
+
+	private setPositionState() {
+		navigator.mediaSession.setPositionState({
+			duration: this.el.duration,
+			position: this.el.currentTime,
+			playbackRate: this.el.playbackRate
+		});
 	}
 
 }
