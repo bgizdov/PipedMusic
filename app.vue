3296afe8510c6df33a25f1a127821b3400b606ab
Filip Znachor <filip@znachor.cz>
2024-05-05 14:24:46 +0200

    Separated Nav into its own file

--- app.vue
+++ app.vue
@@ -1,19 +1,7 @@
 <template>
 	<div>
 
-		<nav>
-			<div class="inner">
-				<NuxtLink to="/" @click="shared.player = false;">
-					<img class="logo" src="/logo.webp">
-				</NuxtLink>
-				<SearchBar />
-				<div class="btn-row btn-row-right" @click="shared.player = false;">
-					<NuxtLink to="/search" class="btn-search">
-						<Icon name="mdi:search" />
-					</NuxtLink>
-				</div>
-			</div>
-		</nav>
+		<Nav />
 
 		<SongMenu />
 
@@ -29,8 +17,6 @@
 </template>
 
 <script lang="ts" setup>
-
-import { shared } from "~/src/frontend/App";
 
 useHead({
 	title: "Piped Music"
---
+++ components/Nav.vue
@@ -0,0 +1,21 @@
+<template>
+	<nav>
+		<div class="inner">
+			<NuxtLink to="/" @click="shared.player = false;">
+				<img class="logo" src="/logo.webp">
+			</NuxtLink>
+			<SearchBar />
+			<div class="btn-row btn-row-right" @click="shared.player = false;">
+				<NuxtLink to="/search" class="btn-search">
+					<Icon name="mdi:search" />
+				</NuxtLink>
+			</div>
+		</div>
+	</nav>
+</template>
+
+<script setup lang="ts">
+
+import { shared } from "~/src/frontend/App";
+
+</script>
