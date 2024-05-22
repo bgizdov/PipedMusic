ae4245275588f488804cc338a7f1a1ab1c01a2a3
Filip Znachor <filip@znachor.cz>
2024-05-22 17:00:23 +0200

    Better loop button icon loading

--- components/buttons/LoopButton.vue
+++ components/buttons/LoopButton.vue
@@ -1,8 +1,8 @@
 <template>
 	<button class="btn-loop" @click="queue.cycleLoop">
-		<Icon v-if="queue.loop == 'none'" name="cil:loop" class="semiopacity" />
-		<Icon v-if="queue.loop == 'playlist'" name="cil:loop" />
-		<Icon v-if="queue.loop == 'one'" name="cil:loop-1" />
+		<Icon :class="{hidden: queue.loop != 'none'}" name="cil:loop" class="semiopacity" />
+		<Icon :class="{hidden: queue.loop != 'playlist'}" name="cil:loop" />
+		<Icon :class="{hidden: queue.loop != 'one'}" name="cil:loop-1" />
 	</button>
 </template>
 
